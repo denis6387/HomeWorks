0b1b1dc55b73019cbc764204674b768511ff879a
denis6387 <denis6387@gmail.com>
2025-03-06 09:22:09 +0700

    Домашняя работа по теме 4

---
+++ Task1_4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите натуральное число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            { 
+                fact = fact * i;
+            }
+            Console.WriteLine("Факториал числа {0} равен {1}", n, fact);
+            Console.ReadKey();
+        }
+    }
+}
